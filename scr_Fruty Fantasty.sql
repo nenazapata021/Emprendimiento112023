1c2dada348c5a9c897bba2eb16376e7493d2cc81
nenazapata021 <97985068+nenazapata021@users.noreply.github.com>
2023-09-05 07:37:15 -0500

    Add files via upload

---
+++ scr_Fruty Fantasty.sql
@@ -0,0 +1,101 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: bzvfyoi00f4pgv0w9r7w-mysql.services.clever-cloud.com:3306
+-- Tiempo de generación: 05-09-2023 a las 12:33:04
+-- Versión del servidor: 8.0.22-13
+-- Versión de PHP: 8.2.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `bzvfyoi00f4pgv0w9r7w`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tblproductos`
+--
+
+CREATE TABLE `tblproductos` (
+  `id` int NOT NULL,
+  `Nombre` varchar(20) NOT NULL,
+  `Precio` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `tblproductos`
+--
+
+INSERT INTO `tblproductos` (`id`, `Nombre`, `Precio`) VALUES
+(1, 'Fresa con chocolate', '3.000'),
+(2, 'Banano con chocolate', '2.500'),
+(3, 'Manzana-chocolate', '3.500');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tblusuario`
+--
+
+CREATE TABLE `tblusuario` (
+  `id` int NOT NULL,
+  `nombre` varchar(30) NOT NULL,
+  `estado` varchar(30) NOT NULL,
+  `clave` varchar(40) NOT NULL,
+  `correo` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `tblusuario`
+--
+
+INSERT INTO `tblusuario` (`id`, `nombre`, `estado`, `clave`, `correo`) VALUES
+(1, '', '', 'AN2c4J9r6H8Jo', 'andres021@gmail.com');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `tblproductos`
+--
+ALTER TABLE `tblproductos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `tblusuario`
+--
+ALTER TABLE `tblusuario`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `tblproductos`
+--
+ALTER TABLE `tblproductos`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `tblusuario`
+--
+ALTER TABLE `tblusuario`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
